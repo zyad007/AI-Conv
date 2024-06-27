b74f46bc79797b3857acadd6d796924e5a6e585b
anas12312 <anashesham000@gmail.com>
2024-06-27 17:52:45 +0300

    anas

--- src/Components/Chat.tsx
+++ src/Components/Chat.tsx
@@ -6,7 +6,7 @@ import Response from './Response';
 const Chat = () => {
     const nav = useNavigate();
     const params = useParams();
-
+    
     return (
         <div className='w-full h-full flex flex-col justify-end items-start overflow-y-scroll p-6'>
             <Message />

c90d2bca4c8fce010d5e9279c3c2dd411910592f
unknown <zyad.sallem.007@gmail.com>
2024-06-30 07:31:06 +0300

    z

--- src/Modals/CreateBotModal.tsx
+++ src/Modals/CreateBotModal.tsx
@@ -12,8 +12,33 @@ export default function CreateBotModal({ isOpen, setIsOpen }
     const [name, setName] = useState('')
     const [instruction, setInstruction] = useState('')
 
+    const [botsList, setBotsList] = useState<{ id: string, name: string }[]>([])
+
     const nav = useNavigate()
     const params = useParams()
+
+    useEffect(() => {
+        loadBots()
+    }, [])
+
+    const loadBots = () => {
+        fetch('http://localhost:3000/bot')
+            .then(res => res.json())
+            .then(result => {
+                setBotsList(result)
+            })
+    }
+
+    const handleDelete = (id: string) => {
+        fetch('http://localhost:3000/bot/' + id, {
+            method: 'DELETE'
+        })
+            .then(x => {
+                if (x.ok) {
+                    loadBots()
+                }
+            })
+    }
 
     const handleCreateChat = () => {
         fetch('http://localhost:3000/bot', {
@@ -26,14 +51,14 @@ export default function CreateBotModal({ isOpen, setIsOpen }
                 instruction
             })
         })
-        .then(res => {
-            if(res.ok) {
-                closeModal();
-            }
-        })
-        .catch(e => {
-            console.log(e);
-        }) 
+            .then(res => {
+                if (res.ok) {
+                    closeModal();
+                }
+            })
+            .catch(e => {
+                console.log(e);
+            })
     }
 
     const closeModal = () => {
@@ -42,39 +67,77 @@ export default function CreateBotModal({ isOpen, setIsOpen }
         setIsOpen(false);
     }
 
+
+
     return (
         <div>
             <ReactModal
                 appElement={document.getElementById('root')}
                 isOpen={isOpen}
-                className={'-translate-x-1/3 shadow-xl shadow-slate-300 -translate-y-1/3 left-1/2 top-1/2 h-64 w-96 absolute flex flex-col justify-around items-center border p-5 rounded-md bg-secondary-2 text-black'}
+                className={'-translate-x-1/3 shadow-xl shadow-slate-300 bg-slate-200 -translate-y-1/3 left-[37%] top-1/3 h-[80%] w-[80%] absolute border p-5 rounded-md bg-secondary-2 text-black'}
                 shouldFocusAfterRender={false}
                 onRequestClose={closeModal}
                 closeTimeoutMS={200}
             >
-                <div className='w-full'>
-                    <div><label htmlFor="Chat-Name" id='Chat-Name-Lable' className='text-sm ml-1'>Bot Name</label></div>
-                    <input id='Chat-Name' className='text-sm w-full my-1 h-8 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 ' type='text' value={name}
-                        onChange={(e) => {
-                            setName(e.target.value)
-                        }}
-                    />
-                </div>
+                <div className="w-full h-full flex justify-center items-center space-x-3">
 
-                <div className='w-full'>
-                    <div><label htmlFor="Chat-Name" id='Chat-Name-Lable' className='text-sm ml-1'>Bot Instructions</label></div>
-                    <input id='Chat-Name' className='text-sm w-full my-1 h-8 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 ' type='text' value={instruction}
-                        onChange={(e) => {
-                            setInstruction(e.target.value)
-                        }}
-                    />
-                </div>
+                    <div className="w-full h-full space-y-2 overflow-y-scroll pr-1">
 
-                <button
-                    onClick={() => { handleCreateChat() }}
-                    className="w-full  h-9 rounded-md border text-sm bg-slate-800 text-white hover:bg-slate-600">
-                    Create
-                </button>
+                        {
+                            botsList.map(x =>
+                                <div className="relative h-24 w-full bg-white flex justify-center items-start pl-10 flex-col">
+
+                                    <div className="font-bold text-xl truncate w-full pr-20">
+                                        {x.name}
+                                    </div>
+
+                                    <div className="font-semibold">
+                                        Id: {x.id}
+                                    </div>
+
+                                    <div className="absolute right-0 p-5">
+                                        <div className="text-red-700 hover:underline select-none hover:cursor-pointer" onClick={() => handleDelete(x.id)}>
+                                            Delete
+                                        </div>
+                                    </div>
+
+                                </div>
+                            )
+                        }
+
+
+                    </div>
+
+                    <div className="w-full h-full flex justify-center items-center flex-col space-y-10">
+
+                        <div className='w-full'>
+                            <div><label htmlFor="Chat-Name" id='Chat-Name-Lable' className='text-sm ml-1'>Bot Name</label></div>
+                            <input id='Chat-Name' className='text-sm w-full h-8 py-1 px-2 border rounded-md bg-secondary-3 ' type='text' value={name}
+                                onChange={(e) => {
+                                    setName(e.target.value)
+                                }}
+                            />
+                        </div>
+
+                        <div className='w-full h-full'>
+                            <div><label htmlFor="Chat-Instructions" id='Chat-Instructions-Lable' className='text-sm ml-1'>Bot Instructions</label></div>
+                            <textarea id='Chat-Instructions' className='text-sm w-full h-full py-1 px-2 border rounded-md bg-secondary-3 resize-none' value={instruction}
+                                onChange={(e) => {
+                                    setInstruction(e.target.value)
+                                }}
+                            />
+                        </div>
+
+                        <div className="w-full">
+                            <button
+                                onClick={() => { handleCreateChat() }}
+                                className="w-full h-9 rounded-md border text-sm bg-slate-800 text-white hover:bg-slate-600">
+                                Create
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
             </ReactModal>
         </div>
     )
